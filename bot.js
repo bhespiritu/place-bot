574a003ee819ee74281ee4faa7b9ea31c58b99a6
Block Head <lozman1@elne.org>
2019-09-08 22:36:24 -0400

    first commit

---
+++ bot.js
@@ -0,0 +1,50 @@
+const Discord = require('discord.js');
+var Jimp = require('jimp');
+var auth = require('./auth.json');
+const client = new Discord.Client();
+const fileOutput = './place.png';
+const scaleOutput = './scale.png';
+const { Client, MessageAttachment } = require('discord.js');
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+	if(msg.author.bot) return;
+	var args = msg.content.split(/\s+/);
+	console.log(args);
+	if(args.length >= 5)
+	{
+		var x = parseFloat(args[0]);
+		var y = parseFloat(args[1]);
+		var r = 255 - parseInt(args[2]);
+		var g = 255 - parseInt(args[3]);
+		var b = 255 - parseInt(args[4]);
+		if(x != NaN && y != NaN && r != NaN && g != NaN && b != NaN)
+		{
+			if(x >= 0 && x <100 && y >= 0 && y < 100)
+			{
+				
+				console.log({x,y,r,g,b});
+				var hex = Jimp.rgbaToInt(r, g, b, 255);
+				Jimp.read(fileOutput)
+				  .then(image => {
+					image.setPixelColor(hex, x, y);
+					image.write(fileOutput);
+					image.scale(2,Jimp.RESIZE_NEAREST_NEIGHBOR);
+					image.write(scaleOutput);
+				  }).then().then(()=>(msg.channel.send(``, {file: scaleOutput}))).catch(err => {
+					console.error(err)
+				  });
+				  
+			
+					
+				
+			}
+		}
+	}
+});
+
+client.login(auth.token);
